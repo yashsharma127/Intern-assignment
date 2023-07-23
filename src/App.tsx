e0dbe5ae2d6f275b24d727ccb2023b9dd273448f
yashsharma127 <yashshar.2323@gmail.com>
2023-07-23 14:37:47 +0530

    adding routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Form from './components/Form'
+import Page2 from './components/Page2';
 
 
 const App = () => {
   return (
-    <div>
-      <Form />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Form />} />
+        <Route path="/components/Page2" element={<Page2 />} />
+      </Routes>
+    </Router>
   );
 };
 
--- src/components/Page2.tsx
+++ src/components/Page2.tsx
@@ -1,5 +1,9 @@
 const Page2 = () => {
-  
+  return(
+    <div>
+        hello
+    </div>
+  )
 }
 
 export default Page2

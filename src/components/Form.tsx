2ef0d7ae71c120100e5d18bf136b4ba4479f5161
yashsharma127 <yashshar.2323@gmail.com>
2023-07-23 14:05:46 +0530

    updating form component

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,18 +1,42 @@
 import { TextField, Button, Container, Typography, Box } from '@mui/material';
-
+import  { useState } from 'react'
 
 
 const Form = () => {
+
+  //form data storing state
+  const [formData, setFormData] = useState({
+    name: '',
+    phoneNumber: '',
+    email: '',
+  });
+
+
+  //handel changes in input fields
+  const handleChange = (event: { target: { name: any; value: any; }; }) => {
+    const { name, value } = event.target;
+
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  };
+
+
+  //handel changes occurs when subit button is clicked
+  const handleSubmit = async ( event: { preventDefault: () => void; } ) => {
+    event.preventDefault()
+  };
+
   
     return (
         <Container maxWidth="sm">
       <Box mt={5}>
         <Typography variant="h4">User Information</Typography>
-        <form >
+        <form onSubmit={handleSubmit}>
           <TextField
             fullWidth
             label="Name"
             name="name"
+            value={formData.name}
+            onChange={handleChange}
             margin="normal"
             variant="outlined"
             required
@@ -21,6 +45,8 @@ const Form = () => {
             fullWidth
             label="Phone Number"
             name="phoneNumber"
+            value={formData.phoneNumber}
+            onChange={handleChange}
             margin="normal"
             variant="outlined"
             required
@@ -29,6 +55,8 @@ const Form = () => {
             fullWidth
             label="Email"
             name="email"
+            value={formData.email}
+            onChange={handleChange}
             margin="normal"
             variant="outlined"
             required

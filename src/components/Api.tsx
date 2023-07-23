de33e884438c48c970631830155719f013766f6a
yashsharma127 <yashshar.2323@gmail.com>
2023-07-23 15:34:07 +0530

    added grid to the data fetched from api

--- src/components/Api.tsx
+++ src/components/Api.tsx
@@ -1,16 +1,23 @@
 import axios from "axios";
 import { useEffect, useState } from 'react';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 
+interface Post {
+    id: number;
+    userId: number;
+    title: string;
+    body: string;
+  }
 
 const Api = () => {
 
-    const [data, setData] = useState([]);
+    const [data, setData] = useState<Post[]>([]);
 
     useEffect(() => {
         // Function to fetch data from the API
         const fetchData = async () => {
           try {
-            const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+            const response = await axios.get<Post[]>('https://jsonplaceholder.typicode.com/posts');
             setData(response.data);
           } catch (error) {
             console.error('Error while fetching data:', error);
@@ -21,12 +28,18 @@ const Api = () => {
         fetchData();
       }, []);
     
-      // Define columns for the data grid
+       // Define columns for the data grid
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'userId', headerName: 'User ID', width: 120 },
+    { field: 'title', headerName: 'Title', width: 300 },
+    { field: 'body', headerName: 'Body', width: 400 },
+  ];
   
 
     return(
-        <div>
-            {JSON.stringify(data)}
+        <div style={{ height: 400, width: '100%' }}>
+            <DataGrid rows={data} columns={columns} checkboxSelection />
         </div>
     )
 }
--- src/components/Page2.tsx
+++ src/components/Page2.tsx
@@ -6,12 +6,12 @@ const Page2 = () => {
   return (
     <div style={{ height: 400, width: '100%' }}>
       <h1>Data fetched from the API:</h1>
-      
+      <Api />
 
       &nbsp;
 
       <h1>Department List</h1>
-      <Api />
+      
     </div>
   );
 };

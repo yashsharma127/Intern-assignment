7709816c5c4624637d3974adb2536d0e35ec4bc3
yashsharma127 <yashshar.2323@gmail.com>
2023-07-23 15:24:43 +0530

    fetching api data to page2

--- src/components/Api.tsx
+++ src/components/Api.tsx
@@ -1,5 +1,34 @@
+import axios from "axios";
+import { useEffect, useState } from 'react';
+
+
 const Api = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        // Function to fetch data from the API
+        const fetchData = async () => {
+          try {
+            const response = await axios.get('https://jsonplaceholder.typicode.com/posts');
+            setData(response.data);
+          } catch (error) {
+            console.error('Error while fetching data:', error);
+          }
+        };
+    
+        // Call the fetchData function when the component mounts
+        fetchData();
+      }, []);
+    
+      // Define columns for the data grid
   
+
+    return(
+        <div>
+            {JSON.stringify(data)}
+        </div>
+    )
 }
 
 export default Api
--- src/components/Page2.tsx
+++ src/components/Page2.tsx
@@ -1,9 +1,19 @@
-const Page2 = () => {
-  return(
-    <div>
-        hello
-    </div>
-  )
-}
+import Api from "./Api";
 
-export default Page2
+const Page2 = () => {
+  
+
+  return (
+    <div style={{ height: 400, width: '100%' }}>
+      <h1>Data fetched from the API:</h1>
+      
+
+      &nbsp;
+
+      <h1>Department List</h1>
+      <Api />
+    </div>
+  );
+};
+
+export default Page2;
